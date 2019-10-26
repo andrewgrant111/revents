fc4d2a4cd414126bc47f410d75ff373d556ae0d9
andrewgrant111 <andrewgrant111@gmail.com>
2019-10-26 15:13:09 -0700

    End of section 7

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -9,6 +9,7 @@ import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard
 import UserDetailedPage from "../../features/user/UserDetailed/UserDetailedPage";
 import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
 import EventForm from "../../features/event/EventForm/EventForm";
+import TestComponent from "../../features/testarea/TestComponent";
 
 class App extends Component {
   render() {
@@ -27,6 +28,7 @@ class App extends Component {
                 <Route path='/profile/:id' component={UserDetailedPage}></Route>
                 <Route path='/settings' component={SettingsDashboard}></Route>
                 <Route path='/createEvent' component={EventForm}></Route>
+                <Route path='/test' component={TestComponent}></Route>
               </Container>
             </Fragment>
           )}
